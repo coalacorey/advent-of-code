7d9071c307f2a0504865d2897e2647ba71b2f87c
Corey Kreiniker <coalacorey@gmail.com>
2022-12-04 14:19:27 +0100

    Added solution for day 04.

---
+++ day_04/solution_04.py
@@ -0,0 +1,35 @@
+def main():
+    print("--- Day 4: Camp Cleanup ---")
+    input = open('input.txt', 'r')
+    lines = input.readlines()
+    lines_copy = lines.copy()
+    print("Solution part 1: " + str(solve_part_1(lines)))
+    print("Solution part 2: " + str(solve_part_2(lines)))
+
+
+def solve_part_1(lines):
+    res = 0
+    for line in lines:
+        one = set(range(int(line.split(',')[0].split(
+            '-')[0]), int(line.split(',')[0].split('-')[1]) + 1))
+        two = set(range(int(line.split(',')[1].split(
+            '-')[0]), int(line.split(',')[1].split('-')[1]) + 1))
+        if one <= two or two <= one:
+            res += 1
+    return res
+
+
+def solve_part_2(lines):
+    res = 0
+    for line in lines:
+        one = set(range(int(line.split(',')[0].split(
+            '-')[0]), int(line.split(',')[0].split('-')[1]) + 1))
+        two = set(range(int(line.split(',')[1].split(
+            '-')[0]), int(line.split(',')[1].split('-')[1]) + 1))
+        if one & two:
+            res += 1
+    return res
+
+
+if __name__ == '__main__':
+    main()
