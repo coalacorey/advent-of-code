a362b65b18b5a5bab56632c7ba5deed2a18ec867
Corey Kreiniker <coalacorey@gmail.com>
2022-12-11 15:40:05 +0100

    Turned functions into hardcoded lists

--- day_11/solution_11.py
+++ day_11/solution_11.py
@@ -9,6 +9,8 @@ def main():
 
 def solve_part_1():
     monkey_inspections = [0, 0, 0, 0, 0, 0, 0, 0]
+    monkey_modulos = [11, 5, 19, 13, 7, 17, 2, 3]
+    monkey_partners = [[2, 3], [4,0], [5,6], [2,6], [0,3], [7,1], [7,5], [4,1]]
     monkeys = get_monkeys()
     for _ in range(0, 20):
         for monkey in range(8):
@@ -17,8 +19,10 @@ def solve_part_1():
                 wl = get_worry_level_after_inspection(
                     monkeys[monkey].pop(0), monkey)
                 wl = int(math.floor(wl/3))
-                receiving_monkey = get_receiving_monkey(
-                    get_modulo_for_monkey(wl, monkey), monkey)
+                if wl % monkey_modulos[monkey] == 0:
+                    receiving_monkey = monkey_partners[monkey][0]
+                else:
+                    receiving_monkey = monkey_partners[monkey][1]
                 monkeys[receiving_monkey].append(wl)
     monkey_inspections.sort(reverse=True)
     return monkey_inspections[0] * monkey_inspections[1]
@@ -26,6 +30,8 @@ def solve_part_1():
 
 def solve_part_2():
     monkey_inspections = [0, 0, 0, 0, 0, 0, 0, 0]
+    monkey_modulos = [11, 5, 19, 13, 7, 17, 2, 3]
+    monkey_partners = [[2, 3], [4,0], [5,6], [2,6], [0,3], [7,1], [7,5], [4,1]]
     monkeys = get_monkeys()
     lcm = math.lcm(11, 5, 19, 13, 7, 17, 2, 3)
     for _ in range(0, 10000):
@@ -34,8 +40,10 @@ def solve_part_2():
                 monkey_inspections[monkey] = monkey_inspections[monkey] + 1
                 level = monkeys[monkey].pop(0)
                 wl = get_worry_level_after_inspection(level, monkey)
-                receiving_monkey = get_receiving_monkey(
-                    get_modulo_for_monkey(wl, monkey), monkey)
+                if wl % monkey_modulos[monkey] == 0:
+                    receiving_monkey = monkey_partners[monkey][0]
+                else:
+                    receiving_monkey = monkey_partners[monkey][1]
                 monkeys[receiving_monkey].append(wl % lcm)
     monkey_inspections.sort(reverse=True)
     return monkey_inspections[0] * monkey_inspections[1]
@@ -60,71 +68,6 @@ def get_worry_level_after_inspection(old, monkey):
     elif monkey == 7:
         new = old + 7
     return new
-
-
-def get_modulo_for_monkey(item, monkey):
-    if monkey == 0:
-        new = item % 11
-    elif monkey == 1:
-        new = item % 5
-    elif monkey == 2:
-        new = item % 19
-    elif monkey == 3:
-        new = item % 13
-    elif monkey == 4:
-        new = item % 7
-    elif monkey == 5:
-        new = item % 17
-    elif monkey == 6:
-        new = item % 2
-    elif monkey == 7:
-        new = item % 3
-    return new
-
-
-def get_receiving_monkey(item, monkey):
-    receiving_monkey = 0
-    if monkey == 0:
-        if item == 0:
-            receiving_monkey = 2
-        else:
-            receiving_monkey = 3
-    elif monkey == 1:
-        if item == 0:
-            receiving_monkey = 4
-        else:
-            receiving_monkey = 0
-    elif monkey == 2:
-        if item == 0:
-            receiving_monkey = 5
-        else:
-            receiving_monkey = 6
-    elif monkey == 3:
-        if item == 0:
-            receiving_monkey = 2
-        else:
-            receiving_monkey = 6
-    elif monkey == 4:
-        if item == 0:
-            receiving_monkey = 0
-        else:
-            receiving_monkey = 3
-    elif monkey == 5:
-        if item == 0:
-            receiving_monkey = 7
-        else:
-            receiving_monkey = 1
-    elif monkey == 6:
-        if item == 0:
-            receiving_monkey = 7
-        else:
-            receiving_monkey = 5
-    elif monkey == 7:
-        if item == 0:
-            receiving_monkey = 4
-        else:
-            receiving_monkey = 1
-    return receiving_monkey
 
 
 def get_monkeys():
