f3970ab131a94a4c4f399c2f039b33fddf66c099
Corey Kreiniker <coalacorey@gmail.com>
2022-12-13 11:48:44 +0100

    Replaced deepcopy use by tuples for better performance

--- day_13/solution_13.py
+++ day_13/solution_13.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
-import copy
 import functools
 
 
 def main():
     print("--- Day 13: Distress Signal ---")
-    datastream = open('input.txt', 'r').readlines()
-    print("Solution part 1: " + str(solve_part_1(copy.deepcopy(datastream))))
-    print("Solution part 2: " + str(solve_part_2(copy.deepcopy(datastream))))
+    datastream = tuple(open('input.txt', 'r').readlines())
+    print("Solution part 1: " + str(solve_part_1(datastream)))
+    print("Solution part 2: " + str(solve_part_2(datastream)))
 
 
 def solve_part_1(datastream) -> int:
@@ -31,21 +30,21 @@ def solve_part_2(datastream):
     signal_pairs = []
     for i in range(len(datastream)):
         if len(datastream[i].strip()) > 0:
-            signal_pairs.append(eval(str(datastream[i].strip())))
+            signal_pairs.append(tuple(eval(str(datastream[i].strip()))))
 
-    signal_pairs.append([[2]])
-    signal_pairs.append([[6]])
+    signal_pairs.append(tuple([[2]]))
+    signal_pairs.append(tuple([[6]]))
 
     s = sorted(signal_pairs, key=functools.cmp_to_key(
         compare_pair_ordering), reverse=True)
-    index_1 = s.index([[2]]) + 1
-    index_2 = s.index([[6]]) + 1
+    index_1 = s.index(tuple([[2]])) + 1
+    index_2 = s.index(tuple([[6]])) + 1
     return index_1*index_2
 
 
 def compare_pair_ordering(in_left, in_right):
-    left = copy.deepcopy(in_left)
-    right = copy.deepcopy(in_right)
+    left = list(in_left)
+    right = list(in_right)
     # Check if left list is not empty
     while len(left) > 0:
         # Check if right list is not empty, otherwise return false because it ran out of list elements first
