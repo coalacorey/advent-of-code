45d1323b2f00e0c6fdd0ea04950e57b9a49b9555
Corey Kreiniker <coalacorey@gmail.com>
2022-12-17 00:08:15 +0100

    Added solution for part 2 day 16

--- day_16/solution_16.py
+++ day_16/solution_16.py
@@ -7,7 +7,7 @@ from itertools import permutations
 
 MOVE_TIME = 1
 VALVE_OPEN_TIME = 1
-TIME_LIMIT_1 = 30
+TIME_LIMIT_1 = 26
 
 
 @functools.lru_cache(maxsize=None)
@@ -16,7 +16,7 @@ def main():
     input = tuple(open('input.txt', 'r').readlines())
     parse_input(input)
     print("Solution part 1: " + str(solve_part_1(input)))
-    # print("Solution part 2: " + str(solve_part_2(input)))
+    print("Solution part 2: " + str(solve_part_2(input)))
 
 
 @functools.lru_cache(maxsize=None)
@@ -27,7 +27,6 @@ def solve_part_1(input):
     current_path = [v for v in valve_flow_rate.keys()
                     if valve_flow_rate[v] > 0]
 
-    # get_distance.counter = 0
     high_score = 0
     for i in range(2, len(current_path) + 1):
         lp = permutations(current_path, i)
@@ -39,7 +38,6 @@ def solve_part_1(input):
                 high_score = score
             permutation = next(lp, None)
         print(str(i) + ": " + str(high_score))
-        # print(get_distance.counter)
 
     return high_score
 
@@ -63,7 +61,7 @@ def simulate(path, graph, valve_visited, valve_flow_rate, time_limit):
                 distance = get_distance(graph, current_valve, target_valve)
                 for i in range(1, distance + VALVE_OPEN_TIME + 1):
                     if i == distance + 1:
-                        valve_visited[target_valve] = True
+                        # valve_visited[target_valve] = True
                         pressure_per_minute += valve_flow_rate[target_valve]
                     minute += 1
                     # print("== Minute " + str(abs(time)) + " ==\n")
@@ -88,7 +86,6 @@ def populate_graph(v_c, graph: dijkstra.Graph):
 
 @functools.lru_cache(maxsize=None)
 def get_distance(graph, start_valve, target_valve):
-    # get_distance.counter += 1
     d_search = dijkstra.DijkstraSPF(graph, start_valve)
     distance = d_search.get_distance(target_valve)
     return distance
@@ -110,8 +107,43 @@ def parse_input(input):
 
 
 def solve_part_2(input):
-    return 0
+    valve_connections, valve_flow_rate, valve_visited = parse_input(input)
+    graph = dijkstra.Graph()
+    graph = populate_graph(valve_connections, graph)
+    current_path = [v for v in valve_flow_rate.keys()
+                    if valve_flow_rate[v] > 0]
 
+    high_score_1 = 0
+    solution_path = []
+    for i in range(2, len(current_path)):
+        lp = permutations(current_path, i)
+        permutation = next(lp, None)
+        while permutation != None:
+            score = simulate(permutation, graph,
+                             valve_visited, valve_flow_rate, TIME_LIMIT_1)
+            if score > high_score_1:
+                high_score_1 = score
+                solution_path = permutation
+            permutation = next(lp, None)
+    print("Me: " + str(high_score_1))
+
+    for item in solution_path:
+        if item in current_path:
+            current_path.remove(item)
+
+    high_score_2 = 0
+    for i in range(2, len(current_path) + 1):
+        lp = permutations(current_path, i)
+        permutation = next(lp, None)
+        while permutation != None:
+            score = simulate(permutation, graph,
+                             valve_visited, valve_flow_rate, TIME_LIMIT_1)
+            if score > high_score_2:
+                high_score_2 = score
+            permutation = next(lp, None)
+    print("Elefant: " + str(high_score_2))
+
+    return high_score_1 +  high_score_2
 
 if __name__ == '__main__':
     main()
